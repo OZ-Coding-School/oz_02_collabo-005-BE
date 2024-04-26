a9f73e5a1a26d152a4ccbd32c41ca5ce5a42c794
포도주인 <155045987+KangJeongHo1@users.noreply.github.com>
2024-04-26 13:23:39 +0900

    Feat: User 정보 받아오는 get 메서드 추가 #15 (#20)
    * Feat: UserCreate, EmailCheck View 생성 및 urlpatterns 작성 (#15)
    
    * Feat: login API 개발 (#15)
    
    * Feat: Login, Logout, Update API 작성 (#15)
    
    * Feat: Delete API 작성 (#15)
    
    * Feat: LoginView 수정 및 주석 추가 (#15)
    
    * Feat: 유저정보 받아오는 get 메서드 추가 (#15)
    
    close: #15

--- order/tests.py
+++ order/tests.py
@@ -1,34 +1,3 @@
 from django.test import TestCase
-from .models import Order
-from user.models import User 
 
-class OrderModelTestCase(TestCase):
-    def setUp(self):
-        """테스트에 필요한 사용자 생성"""
-        self.user = User.objects.create(name="test_user", email="test@example.com")
-
-    def test_create_order(self):
-        """Order 생성 테스트"""
-        order = Order.objects.create(
-            user=self.user,
-            order_status=1,
-            delivery_address="123 Street, City",
-            total_price=10000,
-        )
-
-        """생성된 Order가 제대로 저장되었는지 확인"""
-        self.assertEqual(Order.objects.count(), 1)
-        self.assertEqual(order.order_status, 1)
-        self.assertEqual(order.delivery_address, "123 Street, City")
-        self.assertEqual(order.total_price, 10000)
-        self.assertEqual(order.user, self.user) 
-
-    def test_order_str_representation(self):
-        """Order 객체의 문자열 표현 확인"""
-        order = Order.objects.create(
-            user=self.user,
-            order_status=1,
-            delivery_address="123 Street, City",
-            total_price=10000,
-        )
-        self.assertEqual(str(order), f"Order #{order.pk}")
+# Create your tests here.
