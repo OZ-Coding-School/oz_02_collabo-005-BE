b19b3b254b8769dab940ef9be16cb502b8aa0baf
포도주인 <155045987+KangJeongHo1@users.noreply.github.com>
2024-04-29 10:44:25 +0900

    Feat: 주소 생성 API 작성 (#22) (#26)

--- user/models.py
+++ user/models.py
@@ -69,7 +69,7 @@ class User(CommonModel, PermissionsMixin, AbstractBaseUser):
 class Address(CommonModel):
     """사용자의 주소를 나타내는 주소 모델"""
 
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, default="home")
     base = models.CharField(max_length=255)
     detail = models.CharField(max_length=255)
 
