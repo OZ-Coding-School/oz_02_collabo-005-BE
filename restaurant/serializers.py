b30601eacb0e013984011da860b0fd5c39b681ce
포도주인 <155045987+KangJeongHo1@users.noreply.github.com>
2024-04-30 16:59:57 +0900

    Feat: 가게 리스트 조회 API 개발 (#33) (#34)

---
+++ restaurant/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Restaurant
+
+class RestaurantSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Restaurant
+        fields = ["__all__"]
