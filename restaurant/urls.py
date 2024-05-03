4a6d550f765624a4a4312bcf484f950133319114
[Kang jeongho] <rkdwjdgh04@naver.com>
2024-05-03 10:27:49 +0900

    Feat: 메뉴별 옵션 데이터 가져오는 기능 개발(#39)

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path("list/", RestaurantGetListView.as_view(), name="restaurant-list"),
     # api/v1/restaurant/menu/list?restaurantId=1
     path("menu/list", RestaurantGetDetailView.as_view(), name="restaurant-detail"),
+    # api/v1/restaurant/option/list?menuId=1
+    path("option/list", TestView.as_view(), name="option-detail"),
 ]
