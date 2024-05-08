cf830f9f747835d971c6309452a782363c15a399
포도주인 <155045987+KangJeongHo1@users.noreply.github.com>
2024-05-08 18:27:18 +0900

    Feat: 주문 데이터 만들기(#42) (#55)

--- config/urls.py
+++ config/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/v1/user/", include('user.urls')),
-    path("api/v1/restaurant/", include('restaurant.urls')),
+    path("api/v1/user/", include("user.urls")),
+    path("api/v1/restaurant/", include("restaurant.urls")),
+    path("api/v1/order/", include("order.urls")),
 ]
