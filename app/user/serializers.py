85f6b1c94e6a52c6825917345bd6e95752cab808
newbission <155050120+newbission@users.noreply.github.com>
2024-05-09 19:07:18 +0900

    Push to deploy from dev (#76)
    * Feat: 메뉴의 품절여부 확인 API (#63) (#71)
    
    * Fix: 비밀번호 확인 후 비밀번호 변경 (#71) (#74)
    
    * Fix: migration 파일 수정(?) (#75)
    
    ---------
    
    Co-authored-by: 포도주인 <155045987+KangJeongHo1@users.noreply.github.com>

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -3,9 +3,12 @@ from .models import User, Address
 
 
 class UserSerializer(serializers.ModelSerializer):
+    current_password = serializers.CharField(write_only=True, required=False)
+    new_password = serializers.CharField(write_only=True, required=False)
+
     class Meta:
         model = User
-        fields = ["email", "password", "name", "phone_number", "birthday"]
+        fields = ["email", "password", "name", "phone_number", "birthday", "current_password", "new_password"]
         extra_kwargs = {
             "password": {"write_only": True},  # 패스워드 필드는 읽기 전용으로 설정
             "birthday": {"required": False},
@@ -19,10 +22,13 @@ class UserSerializer(serializers.ModelSerializer):
         return user
 
     def update(self, instance, validated_data):
-        # 비밀번호가 업데이트되는 경우에 대한 처리
-        password = validated_data.pop("password", None)
-        if password:
-            instance.set_password(password)
+        current_password = validated_data.pop('current_password', None)
+        new_password = validated_data.pop('new_password', None)
+
+        if current_password and new_password:
+            if not instance.check_password(current_password):
+                raise serializers.ValidationError({'current_password': '현재 비밀번호가 올바르지 않습니다.'})
+            instance.set_password(new_password)
 
         return super().update(instance, validated_data)
 
