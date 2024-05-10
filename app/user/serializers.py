228ae07a3c6c386c78f0802a3bb733afbf929040
newbission <155050120+newbission@users.noreply.github.com>
2024-05-10 11:34:46 +0900

    변경사항 deploy에 반영 (#80)
    * Feat: 메뉴의 품절여부 확인 API (#63) (#71)
    
    * Fix: 비밀번호 확인 후 비밀번호 변경 (#71) (#74)
    
    * Fix: migration 파일 수정(?) (#75)
    
    * Fix: 레스토랑 데이터 및 유저 비밀번호 업데이트 수정 (#72) (#78)
    
    * Fix: 비밀번호 확인 후 비밀번호 변경 (#71)
    
    * Fix: 레스토랑 데이터 및 유저 비밀번호 업데이트 수정 (#72)
    
    ---------
    
    Co-authored-by: 포도주인 <155045987+KangJeongHo1@users.noreply.github.com>

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -3,12 +3,27 @@ from .models import User, Address
 
 
 class UserSerializer(serializers.ModelSerializer):
-    current_password = serializers.CharField(write_only=True, required=False)
-    new_password = serializers.CharField(write_only=True, required=False)
+
+    current_password = serializers.CharField(
+        write_only=True, required=False, allow_blank=True
+    )
+    new_password = serializers.CharField(
+        write_only=True, required=False, allow_blank=True
+    )
 
     class Meta:
         model = User
-        fields = ["email", "password", "name", "phone_number", "birthday", "current_password", "new_password"]
+        fields = [
+            "email",
+            "password",
+            "name",
+            "phone_number",
+            "birthday",
+            "current_password",
+            "new_password",
+        ]
+        
+
         extra_kwargs = {
             "password": {"write_only": True},  # 패스워드 필드는 읽기 전용으로 설정
             "birthday": {"required": False},
@@ -22,13 +37,24 @@ class UserSerializer(serializers.ModelSerializer):
         return user
 
     def update(self, instance, validated_data):
-        current_password = validated_data.pop('current_password', None)
-        new_password = validated_data.pop('new_password', None)
 
+        current_password = validated_data.pop("current_password", None)
+        new_password = validated_data.pop("new_password", None)
+
+        # current_password와 new_password가 모두 제공되었고, 빈 문자열이 아닌 경우에만 비밀번호 변경 로직 실행
         if current_password and new_password:
             if not instance.check_password(current_password):
-                raise serializers.ValidationError({'current_password': '현재 비밀번호가 올바르지 않습니다.'})
+                raise serializers.ValidationError(
+                    {"current_password": "현재 비밀번호가 올바르지 않습니다."}
+                )
             instance.set_password(new_password)
+        elif current_password or new_password:
+            # current_password 또는 new_password 중 하나만 제공된 경우 오류 발생
+            raise serializers.ValidationError(
+                {"passwords": "현재 비밀번호와 새 비밀번호 모두를 제공해야 합니다."}
+            )
+
+
 
         return super().update(instance, validated_data)
 
