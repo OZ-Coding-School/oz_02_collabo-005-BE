4c826efaa127a0efd73f048dc5de03bcd47c6844
newbission <newbission@gmail.com>
2024-05-11 18:48:44 +0900

    Feat: API url에 등록 (#92)

--- app/okvadmin/urls.py
+++ app/okvadmin/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 from .views import auth_views as av, test_views as tv
 
-# api/v1/user/
 urlpatterns = [
-    # api/v1/admin/auth/
+    ### base: api/v1/admin/ ###
+
+    # auth/
     path("auth/", av.AdminLoginView.as_view(), name="admin-auth"),
+
+    ### restaurant ###
+    # restaurant/list
+    path("restaurant/list", av.AdminLoginView.as_view(), name="admin-restaurant-list"),
+    
 
 
     # 테스트용 API
