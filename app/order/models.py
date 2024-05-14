4570ead479fd103e250ddcc930bdb3295640e61c
Kim <rlawotjq206@gmail.com>
2024-05-14 11:14:53 +0900

    Feat: model 수정(#125)

--- app/order/models.py
+++ app/order/models.py
@@ -81,7 +81,7 @@ class Order_option(CommonModel):
     order_detail = models.ForeignKey(Order_detail, on_delete=models.CASCADE)
     option_name = models.CharField(max_length=255)
     option_price = models.PositiveIntegerField(null=True, default=None)
-    option_group_name = models.CharField(max_length=30)
+    option_group_name = models.CharField(max_length=50)
 
     def __str__(self):
         return f"Order Option - Group: {self.Order_detail.order_group_name}, Name: {self.option_name}, Price: {self.option_price}"
