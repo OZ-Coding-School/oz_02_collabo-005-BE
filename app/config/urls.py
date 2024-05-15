ce753bfe11bd2742e1b3d2aa575d554e049d0b28
[Kang jeongho] <rkdwjdgh04@naver.com>
2024-05-16 01:04:05 +0900

    Feat: config urls수정, poetry파일 수정 (#133)

--- app/config/urls.py
+++ app/config/urls.py
@@ -22,9 +22,9 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, Sp
 
 urlpatterns = [
     # swagger-ui
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/schema/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('api/v1/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/v1/schema/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/v1/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     # applications
     path("admin/", admin.site.urls),
     path("api/v1/user/", include("user.urls")),
