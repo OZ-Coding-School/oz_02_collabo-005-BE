0101bf3619a82a74784026b7abebbabae68d73b1
포도주인 <155045987+KangJeongHo1@users.noreply.github.com>
2024-05-17 14:49:14 +0900

    Feat: order 모델 업데이트 및 주문 예상시간 설정 (#158) (#159)

--- app/okvadmin/views/order_views.py
+++ app/okvadmin/views/order_views.py
@@ -2,12 +2,11 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from order.models import Order
+from order.models import Order, Delivery
 from ..serializers.order_serializers import DummySerializer, OrderApproveSerializer
 from drf_spectacular.utils import extend_schema, OpenApiExample
 from datetime import timedelta
 from django.utils import timezone
-
 
 
 class OrderTestView(APIView):
@@ -29,32 +28,41 @@ class OrderTestView(APIView):
 
 
 class OrderApprove(APIView):
+    """
+    주문 확인 후 소요시간 선택 및 배달확정하는 API
+    """
 
     @extend_schema(
         request=OrderApproveSerializer,
-        responses={200: OrderApproveSerializer()},
+        responses={200: OrderApproveSerializer},
         examples=[
             OpenApiExample(
                 "예시1",
                 summary="배달 승인",
-                description="배달 승인시 cooking_time과 order_status값 변화",
-                value={"id": 1, "refuse": False, "time": 40},
-                request_only=True,  
+                description="배달 승인 시 cooking_time과 order_status 값 변화",
+                value={
+                    "order_id": 1,
+                    "refuse": False,
+                    "time": 40,
+                },
+                request_only=True,
             ),
             OpenApiExample(
                 "예시2",
                 summary="배달 거절",
-                description="배달 거절시 order_status값 변화",
-                value={"id": 1, "refuse": True},
-                request_only=True,  
+                description="배달 거절 시 order_status 값 변화",
+                value={"order_id": 1, "refuse": True},
+                request_only=True,
             ),
         ],
     )
     def post(self, request, format=None):
+        # 요청 데이터에서 필요한 정보 추출
         data = request.data
-        order_id = data.get("id")
+        order_id = data.get("order_id")
         refuse = data.get("refuse")
-        time = data.get("time")
+        time = data.get("time")  # Cooking time
+
 
         try:
             order = Order.objects.get(id=order_id)
@@ -62,17 +70,36 @@ class OrderApprove(APIView):
             return Response(
                 {"message": "Order not found"}, status=status.HTTP_404_NOT_FOUND
             )
+        
+        # 같은 order_id 값이 있을 떄 예외처리 해주기
+        if Delivery.objects.filter(order=order).exists():
+            return Response(
+                {"message": "Order ID already exists"}, status=status.HTTP_400_BAD_REQUEST
+            )
 
         if refuse:
+            # 거부된 경우, 주문 상태 업데이트
             order.order_status = 23
+            order.save()
         else:
+            # 승인된 경우
             current_time_korea = timezone.now()
-            print(current_time_korea)
-            
             cooking_time_delta = timedelta(minutes=time)
             order.cooking_time = current_time_korea + cooking_time_delta
             order.order_status = 21
+            order.save()
 
-        order.save()
+            # 주문에 연결된 배달 정보가 있는지 확인, 없으면 생성
+            if hasattr(order, "delivery"):
+                delivery = order.delivery
+            else:
+                delivery = Delivery(order=order)
+
+            # 배달 정보 업데이트
+            delivery.estimated_time = order.cooking_time + timedelta(
+                minutes=30
+            )
+            delivery.save()
+
         serializer = OrderApproveSerializer(order)
         return Response(serializer.data, status=status.HTTP_200_OK)
